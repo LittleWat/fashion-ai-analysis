c7c1df930d2f186b3f08ad68f45297bf2e6281f0
Kohei Watanabe <oe-watanabe@kddi.com>
2021-02-27 14:00:24 +0900

    wip

--- file_demo.py
+++ file_demo.py
@@ -197,7 +197,7 @@ def main(args):
                         print (str(i+1) + ': ' + sentence)
                         write(detections[i], orig_img, sampled_caption,sentence, i+1, coco_classes, colors)
                         #list(map(lambda x: write(x, orig_img, captions), detections[i].unsqueeze(0)))
-        outfilename = f'/content/drive/MyDrive/models/fashion-ai-analysis/out-{image_filename}.jpg'
+        outfilename = f'{image_filename}.out.jpg'
         print(f"output: {outfilename}")
         cv2.imwrite(outfilename, orig_img)
 
